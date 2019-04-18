47b3ad59f4f277f7d5858b63b8a67b5b8746bd0e
Terrillc13 <terrill.22@wright.edu>
2019-04-18 08:47:00 -0400

    Got jar 1 dispense to work.

--- app.py
+++ app.py
@@ -9,21 +9,48 @@ Blends = Blends()
 
 @app.route('/')
 def webapp():
-    return render_template('home.html',
-        jarFullness1=100, jarFullness2=100, jarFullness3=100)
+    return render_template('home.html', jarFullness1=100, jarFullness2=100, jarFullness3=100)
 
 
 @app.route('/dispense_jar_1', methods=['POST'])
 def dispense_jar_1():
-    print(request.form['Jar-1-Whole'])
-    print(request.form['Jar-1-Fraction'])
-    print("dispense_jar_1")
+    save_home_selects()
+    print(session['Jar-1-Whole'])
+    print(session['Jar-1-Fraction'])
+    print("dispense_jar_1: " + (session['Jar-1-Whole'] + session['Jar-1-Fraction']))
     return webapp()
+
+
+@app.route('/dispense_jar_3', methods=['POST'])
+def dispense_jar_2():
+    save_home_selects()
+    print(session['Jar-2-Whole'])
+    print(session['Jar-2-Fraction'])
+    print("dispense_jar_2: " + (session['Jar-2-Whole'] + session['Jar-2-Fraction']))
+    return webapp()
+
+
+@app.route('/dispense_jar3', methods=['POST'])
+def dispense_jar_3():
+    save_home_selects()
+    print(session['Jar-3-Whole'])
+    print(session['Jar-3-Fraction'])
+    print("dispense_jar_1: " + (session['Jar-3-Whole'] + session['Jar-3-Fraction']))
+    return webapp()
+
+
+def save_home_selects():
+    session['Jar-1-Whole'] = request.form['Jar-1-Whole']
+    session['Jar-1-Fraction'] = request.form['Jar-1-Fraction']
+    session['Jar-2-Whole'] = request.form['Jar-2-Whole']
+    session['Jar-2-Fraction'] = request.form['Jar-2-Fraction']
+    session['Jar-3-Whole'] = request.form['Jar-3-Whole']
+    session['Jar-3-Fraction'] = request.form['Jar-3-Fraction']
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html')s
 
 
 @app.route('/blends')
