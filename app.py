4383b9638ffa381b947cbe1f9b888a5d7c980396
Terrillc13 <terrill.22@wright.edu>
2019-04-20 18:32:11 -0400

    Version 1.0.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, abort
 from PiControl import dispense, fullness
 import xml.etree.cElementTree as Xml
+import os
 
 
 app = Flask(__name__)
@@ -8,6 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def webapp():
+    if not os.path.isfile('spices.xml'):
+        save_spices_to_xml("Spice1", "Item1 - 1/4, Item2 - 2 3/4",
+                           "Spice2", "Item1 - 1/4, Item2 - 2 3/4",
+                           "Spice3", "Item1 - 1/4, Item2 - 2 3/4")
+
     tree = Xml.parse('spices.xml')
     root = tree.getroot()
     return render_template('home.html', jarFullness1=fullness(1), jarFullness2=fullness(2), jarFullness3=fullness(3),
@@ -19,7 +25,6 @@ def webapp():
 @app.route('/dispense_jar_1', methods=['POST'])
 def dispense_jar_1():
     success = dispense(1, (int(request.form['Jar-1-Whole']) + int(request.form['Jar-1-Fraction'])))
-    print("Dispense Jar 1: %d rotations" % (int(request.form['Jar-1-Whole']) + int(request.form['Jar-1-Fraction'])))
     if not success:
         print("Unable to dispense from Jar 1")
     return webapp()
@@ -28,7 +33,6 @@ def dispense_jar_1():
 @app.route('/dispense_jar_2', methods=['POST'])
 def dispense_jar_2():
     success = dispense(2, (int(request.form['Jar-2-Whole']) + int(request.form['Jar-2-Fraction'])))
-    print("Dispense Jar 2: %d rotations" % (int(request.form['Jar-2-Whole']) + int(request.form['Jar-2-Fraction'])))
     if not success:
         print("Unable to dispense from Jar 2")
     return webapp()
@@ -37,7 +41,6 @@ def dispense_jar_2():
 @app.route('/dispense_jar_3', methods=['POST'])
 def dispense_jar_3():
     success = dispense(3, (int(request.form['Jar-3-Whole']) + int(request.form['Jar-3-Fraction'])))
-    print("Dispense Jar 3: %d rotations" % (int(request.form['Jar-3-Whole']) + int(request.form['Jar-3-Fraction'])))
     if not success:
         print("Unable to dispense from Jar 3")
     return webapp()
@@ -48,11 +51,11 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/blends')
-def blends():
+@app.route('/spices')
+def spices():
     tree = Xml.parse('spices.xml')
     root = tree.getroot()
-    return render_template('blends.html',
+    return render_template('spices.html',
                            spice_name_1=root[0][0].attrib['name'],
                            spice_blend_1=root[0][0].text,
                            spice_name_2=root[0][1].attrib['name'],
@@ -61,20 +64,22 @@ def blends():
                            spice_blend_3=root[0][2].text)
 
 
-@app.route('/blends_save', methods=['POST'])
-def blends_save():
-    print("Saving Spice Blends")
-    print("Name = %s ; Blend = %s" % request.form['Spice_1_Name'], request.form['Spice_1_Blend'])
-    print("Name = %s ; Blend = %s" % request.form['Spice_2_Name'], request.form['Spice_2_Blend'])
-    print("Name = %s ; Blend = %s" % request.form['Spice_3_Name'], request.form['Spice_3_Blend'])
+@app.route('/spices_save', methods=['POST'])
+def spices_save():
+    save_spices_to_xml(request.form['Spice_1_Name'], request.form['Spice_1_Blend'],
+                       request.form['Spice_2_Name'], request.form['Spice_2_Blend'],
+                       request.form['Spice_3_Name'], request.form['Spice_3_Blend'])
+    return spices()
+
+
+def save_spices_to_xml(spice_name_1, spice_blend_1, spice_name_2, spice_blend_2, spice_name_3, spice_blend_3):
     root = Xml.Element("root")
     doc = Xml.SubElement(root, "doc")
-    Xml.SubElement(doc, "Spice", name=request.form['Spice_1_Name']).text = request.form['Spice_1_Blend']
-    Xml.SubElement(doc, "Spice", name=request.form['Spice_2_Name']).text = request.form['Spice_2_Blend']
-    Xml.SubElement(doc, "Spice", name=request.form['Spice_3_Name']).text = request.form['Spice_3_Blend']
+    Xml.SubElement(doc, "Spice", name=spice_name_1).text = spice_blend_1
+    Xml.SubElement(doc, "Spice", name=spice_name_2).text = spice_blend_2
+    Xml.SubElement(doc, "Spice", name=spice_name_3).text = spice_blend_3
     tree = Xml.ElementTree(root)
-    tree.write("spices.Xml")
-    return blends()
+    tree.write("spices.xml")
 
 
 if __name__ == '__main__':
